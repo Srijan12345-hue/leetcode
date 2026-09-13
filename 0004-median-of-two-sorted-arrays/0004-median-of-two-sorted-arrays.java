11dc97f9ecec6ab4ae3f0569ee11cbb487a0f7de
srijan naiya <srijannaiya@gmail.com>
2026-09-13 12:42:35 +0530

    Time: 1 ms (100%), Space: 49.1 MB (15.5%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,43 +1,63 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
-        int m = nums1.length;
-        int n = nums2.length;
+        // Make nums1 the smaller array
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
-        int[] ans = new int[m + n];
+        int n = nums1.length;
+        int m = nums2.length;
 
-        int i = 0;
-        int j = 0;
-        int k = 0;
+        int left = 0;
+        int right = n;
 
-        // Merge both sorted arrays
-        while (i < m && j < n) {
+        while (left <= right) {
 
-            if (nums1[i] < nums2[j]) {
-                ans[k++] = nums1[i++];
-            } else {
-                ans[k++] = nums2[j++];
+            int cut1 = left + (right - left) / 2;
+
+            int cut2 = (n + m + 1) / 2 - cut1;
+
+            int left1 = (cut1 == 0)
+                    ? Integer.MIN_VALUE
+                    : nums1[cut1 - 1];
+
+            int right1 = (cut1 == n)
+                    ? Integer.MAX_VALUE
+                    : nums1[cut1];
+
+            int left2 = (cut2 == 0)
+                    ? Integer.MIN_VALUE
+                    : nums2[cut2 - 1];
+
+            int right2 = (cut2 == m)
+                    ? Integer.MAX_VALUE
+                    : nums2[cut2];
+
+            // Correct partition
+            if (left1 <= right2 && left2 <= right1) {
+
+                // Odd
+                if ((n + m) % 2 == 1) {
+                    return Math.max(left1, left2);
+                }
+
+                // Even
+                return (Math.max(left1, left2)
+                        + Math.min(right1, right2)) / 2.0;
+            }
+
+            // nums1 partition is too far right
+            else if (left1 > right2) {
+                right = cut1 - 1;
+            }
+
+            // nums1 partition is too far left
+            else {
+                left = cut1 + 1;
             }
         }
 
-        // Remaining elements of nums1
-        while (i < m) {
-            ans[k++] = nums1[i++];
-        }
-
-        // Remaining elements of nums2
-        while (j < n) {
-            ans[k++] = nums2[j++];
-        }
-
-        int total = m + n;
-
-        // Odd
-        if (total % 2 == 1) {
-            return ans[total / 2];
-        }
-
-        // Even
-        return (ans[total / 2 - 1] + ans[total / 2]) / 2.0;
+        return 0.0;
     }
 }
