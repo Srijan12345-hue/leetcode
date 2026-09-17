4b827fe4148b51f1124d549b0d14f02d3855c196
srijan naiya <srijannaiya@gmail.com>
2026-09-17 15:34:06 +0530

    Time: 1 ms (98.48%), Space: 43.7 MB (6.42%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -2,8 +2,46 @@ class Solution {
     public boolean rotateString(String s, String goal) {
         int n=s.length();
         int m=goal.length();
-        if(n!= m) return false;
-        return (s+ s).contains(goal);
-        
+        if(n != m) return false;
+        String t=(s+s);
+        return kmp(t,goal);
+    }
+    public boolean kmp(String t,String g){
+        int n=t.length();
+        int m=g.length();
+        int [] lps=new int[m];
+        int len=0;
+        int i=1;
+        while(i<m){
+            if(g.charAt(i)==g.charAt(len)){
+                len++;
+                lps[i]=len;
+                i++;
+
+
+            }
+            else{
+                if(len!=0) len=lps[len-1];
+                else{
+                    lps[i]=0;
+                  i++;
+                }
+            }
+        }
+        i=0;
+        int j=0;
+        while(i<n){
+            if(t.charAt(i)==g.charAt(j))
+            {
+                i++;
+                j++;
+                 if(j==m) return true;
+            }
+            else{
+                if(j!=0) j=lps[j-1];
+                else i++;
+            }
+        }
+        return false;
     }
 }
